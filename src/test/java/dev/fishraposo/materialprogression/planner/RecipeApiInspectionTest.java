6326b135354ebe4da27968089afc909c4f2caf79
Vinicius Pereira Raposo <66894455+FishRaposo@users.noreply.github.com>
2026-07-29 05:29:08 -0300

    test: inspect crafting input API

--- src/test/java/dev/fishraposo/materialprogression/planner/RecipeApiInspectionTest.java
+++ src/test/java/dev/fishraposo/materialprogression/planner/RecipeApiInspectionTest.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.fail;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraft.world.item.crafting.CraftingInput;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.PlacementInfo;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.display.RecipeDisplay;
+import net.minecraft.world.item.crafting.display.SlotDisplay;
 import org.junit.jupiter.api.Test;
 
 class RecipeApiInspectionTest {
@@ -18,7 +20,9 @@ class RecipeApiInspectionTest {
         inspect(Ingredient.class);
         inspect(Recipe.class);
         inspect(CraftingRecipe.class);
+        inspect(CraftingInput.class);
         inspect(RecipeDisplay.class);
+        inspect(SlotDisplay.class);
         fail("Intentional one-run target API inspection");
     }
 
